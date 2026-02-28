b040635ff0ea5966e4d4ae2fc71ee6ac7af22dee
kooo12 <agkooo.ako36@gmail.com>
2026-02-28 19:04:55 +0630

    Security: removed api key in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -45,21 +45,22 @@ FutureOr<void> main() async {
     FirebaseOptions? firebaseOptions;
 
     if (Platform.isAndroid) {
-      firebaseOptions = const FirebaseOptions(
-        apiKey: "AIzaSyA1p04lX1jOdHeARyi_SoyCdfkoWQ3QzqA",
-        appId: '1:615822783937:android:8a9e82f7ca58a01d112b40',
-        messagingSenderId: '615822783937',
-        projectId: 'velo-muisc',
-        storageBucket: 'velo-muisc.firebasestorage.app',
+      firebaseOptions = FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_ANDROID_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_ANDROID_APP_ID'] ?? '',
+        messagingSenderId:
+            dotenv.env['FIREBASE_ANDROID_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_ANDROID_PROJECT_ID'] ?? '',
+        storageBucket: dotenv.env['FIREBASE_ANDROID_STORAGE_BUCKET'] ?? '',
       );
     } else if (Platform.isIOS) {
-      firebaseOptions = const FirebaseOptions(
-        apiKey: "AIzaSyAgupPVPyRvPRqDoijHvhPaEymrMY_awcE",
-        appId: '1:615822783937:ios:934f16dc2541bccc112b40',
-        messagingSenderId: '615822783937',
-        projectId: 'velo-muisc',
-        storageBucket: 'velo-muisc.firebasestorage.app',
-        iosBundleId: 'com.ako.velo',
+      firebaseOptions = FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_IOS_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_IOS_APP_ID'] ?? '',
+        messagingSenderId: dotenv.env['FIREBASE_IOS_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_IOS_PROJECT_ID'] ?? '',
+        storageBucket: dotenv.env['FIREBASE_IOS_STORAGE_BUCKET'] ?? '',
+        iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID'] ?? '',
       );
     }
 
